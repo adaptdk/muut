ad39444b408cad26df06b484529fd2203cb297e9
Tom Helmer Hansen <tom@adapt.dk>
2015-02-16 22:49:59 +0100

    Add configuration page

--- js/muut-comments.js
+++ js/muut-comments.js
@@ -1,18 +1,9 @@
 (function ($) {
 	Drupal.behaviors.muut_comments = {
 	  attach: function (context, settings) {
-      Drupal.settings.muut_comments = {};
-		  Drupal.settings.muut_comments.conf = {
-		    login_url: '//thehome.dk/login',
-		    url: "https://muut.com/i/dsrcomments/comments:test",
-		    show_online: true,
-		    upload: false
-		  };
-
-	    $.post( "//dsr.dev/muut-comments/auth", function( data ) {
-	      conf = Drupal.settings.muut_comments.conf;
-	      conf.api  = data.api;
-			  $("#muut-comments").muut(conf);
+	    $.post( "/muut-comments/auth", function( data ) {
+	      Drupal.settings.muut_comments.api  = data.api;
+			  $("#muut-comments").muut(Drupal.settings.muut_comments);
 	    });
 	  }
 	}
