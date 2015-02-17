9212ee9fa79b26d52d6afc3ef927e52f2c4c571f
Tom Helmer Hansen <thelmer@thehome.dk>
2015-02-17 21:57:42 +0100

    Update muut-comments.js

--- js/muut-comments.js
+++ js/muut-comments.js
@@ -1,26 +1,23 @@
 (function ($) {
-	Drupal.behaviors.muut_comments = {
-	  attach: function (context, settings) {
-	  	$('#muut-comments').once('comments', function() {
-		  	var gid = Drupal.settings.ogContext.gid;
-
-		  	$.extend(moot.language, {
-				  online: "Online",
-				  post: "Send",
-				  reply: "Send et svar",
-				  watch: "Følg denne side",
-				  unwatch: "Følg ikke denne side",
-				  unwall: "Nulstil sider der følges",
-				  preview: "Forhåndsvisning",
-				  comment: "Skriv kommentar"
-				});
-
-		    $.post( "/muut-comments/auth/" + gid , function( data ) {
-		      Drupal.settings.muut_comments.api  = data.api;
-		      console.log('X');
-				  $('#muut-comments').muut(Drupal.settings.muut_comments);
-		    });
-	    });
-	  }
-	}
+  Drupal.behaviors.muut_comments = {
+    attach: function (context, settings) {
+      $('#muut-comments').once('comments', function() {
+        var gid = Drupal.settings.ogContext.gid;
+  	$.extend(moot.language, {
+  	  online: "Online",
+  	  post: "Send",
+  	  reply: "Send et svar",
+  	  watch: "Følg denne side",
+  	  unwatch: "Følg ikke denne side",
+  	  unwall: "Nulstil sider der følges",
+  	  preview: "Forhåndsvisning",
+  	  comment: "Skriv kommentar"
+  	});
+  	$.post( "/muut-comments/auth/" + gid , function( data ) {
+  	  Drupal.settings.muut_comments.api  = data.api;
+  	  $('#muut-comments').muut(Drupal.settings.muut_comments);
+  	});
+      });
+    }
+  }
 })(jQuery);
