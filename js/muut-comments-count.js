d862c62640b6698dfc0eeb08c6fb7a74b27e477d
Tom Helmer Hansen <tom@adapt.dk>
2015-02-17 21:53:49 +0100

    Add functionality to enable/disable comments and counter

---
+++ js/muut-comments-count.js
@@ -0,0 +1,16 @@
+(function ($) {
+	Drupal.behaviors.muut_comments_count = {
+	  attach: function (context, settings) {
+	  	$("#muut-comments-count").once('counter', function() {
+		    var url = '//api.muut.com/postcounts?path=/' + Drupal.settings.muut_comments_count.path;
+	      $.getJSON(url, function(json) {
+	        var results = $("#muut-comments-count");
+	        $.each(json, function(key, val) {
+	          results.html(val["size"] + ' kommentarer');
+	        });
+	      });
+	    }
+      );
+	  }
+	}
+})(jQuery);
