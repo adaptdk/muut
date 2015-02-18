874132ebc547e611be464214c939b0e2f1f49071
Andrius <andrius@adapt.dk>
2015-02-18 16:45:28 +0200

    views handler for muut comments count

--- js/muut-comments-count.js
+++ js/muut-comments-count.js
@@ -6,10 +6,24 @@
         $.getJSON(url, function(json) {
           var results = $("#muut-comments-count");
           $.each(json, function(key, val) {
-            results.html(val["size"] + ' kommentarer');
+            results.html(Drupal.t('@count comments', {'@count': val["size"]}));
           });
         });
       });
+
+      $(".muut-comments-count-list-element").each(function(){
+        var results = $(this);
+        results.once('counter', function() {
+          var path = $(this).data('path')
+          var url = '//api.muut.com/postcounts?path=/' + path;
+          $.getJSON(url, function(json) {
+            $.each(json, function(key, val) {
+              results.html(Drupal.t('@count comments', {'@count': val["size"]}));
+            });
+          });
+        });
+      });
+
     }
   }
 })(jQuery);
